ff702a6315adcd6c4b15d5edb29bd6de6d5bee3e
Maruyus <cmariusdaub@hotmail.com>
2022-06-19 00:06:04 +0200

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,61 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useRef } from "react";
+import moon from './resources/moon.png';
+import land from './resources/land.png';
+import cat from './resources/cat.gif';
+
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ref : any = useRef();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Cat!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        {/* <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p> */}
-        {/* <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p> */}
-      </header>
+    <div>
+      <Parallax pages={4} ref={ref}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: 'cover',
+          }}
+        />
+        
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: 'center' }}
+        >
+          <img src={cat} />
+        </ParallaxLayer>
+        
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Hi Cat!</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h2>(I love my Cat!)</h2>
+        </ParallaxLayer>
+      </Parallax>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
